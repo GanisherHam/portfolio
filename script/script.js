894f3765286071596d13858ed2c34e2d9ae2fb90
Gani <ganisherkhamrayev@gmail.com>
2022-04-02 00:57:45 +0500

    refactor: media responsive all sections

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.nav-list');
 const menuExit = document.querySelector('.exit-menu');
 
-burger.addEventListener(`click`, () => {
+burger.addEventListener(`click`, (e) => {
+    e.preventDefault();
     menu.classList.add('active');
     document.body.style.overflow = `hidden`;
 })
 
-menuExit.addEventListener(`click`, () => {
+menuExit.addEventListener(`click`, (e) => {
+    e.preventDefault();
     menu.classList.remove(`active`);
     document.body.style.overflow = ``;
 })
